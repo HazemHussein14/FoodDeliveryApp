e7351c97007d1243890f4976daa5465912a1cd5a
Lamiaa Selim <44843176+lamiaaselim@users.noreply.github.com>
2025-05-21 01:55:05 +0300

    Feature/view and add cart (#10)
    * fix: remove comma exist after last field
    
    * fix: Add a unique constraint to the
    ame column to prevent duplicate names
    
    * feat(entities): Update entities to follow migrations schema
    
    * feat(entities): Add unique constraint to user_type entity
    
    * fix: Remove userId and its association with the user table from the address entity; the address table should no longer contain a userId field.
    
    * feat(entities): Increase phone chars
    
    * fix(entities): Remove address relation
    
    * docs(readme): Update erd
    
    * feat(cart): initial implementation for view and update cart
    
    * feat(cart): initial implementation for view and add cart
    
    * refactor(cart): remove comments in cartService
    
    * refactor(cart): refactor add cart
    
    ---------
    
    Co-authored-by: Randa Magdy <randamagdy123@gmail.com>
    Co-authored-by: Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
    Co-authored-by: Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -4,5 +4,25 @@ import HttpStatusCodes from 'http-status-codes';
 import { CartService } from '../services/cart.service';
 
 export class CartController {
-    private cartService = new CartService();
+	private cartService = new CartService();
+
+	async addCart(req: Request, res: Response) {
+		try {
+			const cart = await this.cartService.createCart(req.body);
+			sendResponse(res, HttpStatusCodes.CREATED, 'Cart created successfully', cart);
+		} catch (error: any) {
+			sendResponse(res, HttpStatusCodes.BAD_REQUEST, 'Failed to create cart', error.message);
+		}
+	}
+
+	async viewCart(req: Request, res: Response) {
+		try {
+			const customerId = parseInt(req.params.customerId);
+			const cart = await this.cartService.viewCart(customerId);
+			sendResponse(res, HttpStatusCodes.OK, 'Cart fetched successfully', cart);
+		} catch (error: any) {
+			sendResponse(res, HttpStatusCodes.NOT_FOUND, 'Cart not found', error.message);
+		}
+	}
+
 }
