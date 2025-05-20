e1fd5f3fcb9673a3b72725f1fe3d443c268bd924
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2025-05-21 01:52:52 +0300

    Feature/remove item from cart (#8)
    * feat: Implement remove item from cart
    
    * feat(cart): Add transaction management for cart item removal
    
    * feat(logging): Add logging to cart item removal
    
    * feat(swagger): Add remove cart item to swagger docs

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -1,7 +1,53 @@
-
-import { CartRepository } from "../repositories/cart.repository";
+import { StatusCodes } from 'http-status-codes';
+import ApplicationError from '../errors/application.error';
+import ErrMessages from '../errors/error-messages';
+import { AppDataSource } from '../config/data-source';
+import { Cart, CartItem } from '../models';
+import logger from '../config/logger';
 
 export class CartService {
-    private cartRepo = new CartRepository() ;
+	async removeItem(cartItemId: number) {
+		logger.info(`Starting removal of cart item`, { cartItemId });
 
+		return await AppDataSource.transaction(async (transactionalEntityManager) => {
+			logger.info(`Beginning transaction for cart item removal`, { cartItemId });
+
+			const cartItem = await transactionalEntityManager.findOne(CartItem, {
+				where: { cartItemId },
+				relations: ['cart']
+			});
+
+			if (!cartItem) {
+				throw new ApplicationError(ErrMessages.cart.CartItemNotFound, StatusCodes.NOT_FOUND);
+			}
+
+			const cart = cartItem.cart;
+			if (!cart) {
+				throw new ApplicationError(ErrMessages.cart.CartNotFound, StatusCodes.NOT_FOUND);
+			}
+
+			// TODO: validate that the customer owns the cart
+
+			if (!cart.isActive) {
+				throw new ApplicationError(ErrMessages.cart.CartNotActive, StatusCodes.BAD_REQUEST);
+			}
+
+			logger.info(`Deleting cart item`, { cartItemId });
+			await transactionalEntityManager.delete(CartItem, cartItemId);
+
+			const cartItems = await transactionalEntityManager.find(CartItem, {
+				where: { cartId: cart.cartId }
+			});
+
+			const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+			await transactionalEntityManager.update(Cart, cart.cartId, { totalItems });
+
+			logger.info(`Successfully removed cart item and updated cart totals`, {
+				cartItemId,
+				cartId: cart.cartId,
+				newTotalItems: totalItems
+			});
+		});
+	}
 }
